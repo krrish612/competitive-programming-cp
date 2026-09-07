6ca9a0426215b0bd72cb98487f34afdaa98cd4cb
Krrish Tripathi <tripathikrrish172@gmail.com>
2026-09-07 14:53:37 +0530

    Add composite number check

--- loop_q16.cpp
+++ loop_q16.cpp
@@ -5,11 +5,11 @@ int main(){
 cout<<"Enter the value of the N':";
 cin>>n;
 f = 1;
-for(int i = 1; i<n; i++){
-    if(n%i==0){
+for(int i = 2; i<n/2; i++){
+    if(n%i==0){ 
         f=i;
     }
 }
-cout<<f;
+cout<<f; 
 return 0;
 }
