8601104bcaf36277464e2b00930b19bd01029509
Krrish Tripathi <tripathikrrish172@gmail.com>
2026-09-07 14:45:08 +0530

    Solve highest factor problem

--- loop_q15.cpp
+++ loop_q15.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int n;
-    cin>>n;
-    
+    int n; int f; 
+cout<<"Enter the value of the N':";
+cin>>n;
+f = 1;
+for(int i = 1; i<n; i++){
+    if(n%i==0){
+        f=i;
+    }
+}
+cout<<f;
+return 0;
 }
